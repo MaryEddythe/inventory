ef35b47095a5c12799186e1f7cac14b7da7396a1
Mary Eddythe <sornito.maryeddythe@gmail.com>
2026-01-05 10:57:28 +0800

    adjusted the signatories

--- resources/views/inventory/export-pdf.blade.php
+++ resources/views/inventory/export-pdf.blade.php
@@ -288,8 +288,8 @@
                 </td>
 
                 <!-- Right cell -->
-                <td class="pdf-col-30 pdf-text-right" style="padding-right: 80px;">
-                    <span class="pdf-signature-label" style="margin-right: 120px;">Reviewed by:</span><br><br>
+                <td class="pdf-col-30 pdf-text-right" style="padding-right: 40px;">
+                    <span class="pdf-signature-label" style="margin-right: 100px;">Reviewed by:</span><br><br>
                     <span class="pdf-signature-name">MAY FLORENCE A. PABELONIO</span><br>
                     <span class="pdf-signature-title" style="margin-right: 50px;">ICT Focal Person</span>
                 </td>

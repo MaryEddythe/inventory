68fd831b6811f22f289ef6641448a3e13bb1464a
Mary Eddythe <sornito.maryeddythe@gmail.com>
2026-06-11 16:46:02 +0800

    motor vehicle

--- app/Models/FurnitureFixture.php
+++ app/Models/FurnitureFixture.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class FurnitureFixture extends Model
 {
-    //
+    protected $table = 'furniture_fixtures';
+
+    protected $fillable = [
+        'article',
+        'description',
+        'property_number',
+        'unit_value',
+        'co_mooe',
+        'date_acquired',
+        'remarks',
+    ];
+
+    protected $casts = [
+        'unit_value' => 'decimal:2',
+        'date_acquired' => 'date',
+    ];
 }

60658ef90a369007bcaa3f063fba8e0c4aa841c7
MaryEddythe <sornito.maryeddythe@gmail.com>
2026-06-15 07:56:54 +0800

    checkpoint hr tokens

--- config/google.php
+++ config/google.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
     'client_id' => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
     'refresh_token' => env('GOOGLE_REFRESH_TOKEN'),
     'folder_id' => env('GOOGLE_DRIVE_FOLDER_ID'),
-
 ];
